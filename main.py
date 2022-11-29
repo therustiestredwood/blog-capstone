43637e814edfea1c1a971b76c609e506d106c659
Morgan Nowak <mzeek88@gmail.com>
2022-11-29 11:42:21 -0500

    Set sqlite as alternative database for local work.

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ gravatar = Gravatar(app,
                     base_url=None)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL") # 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", 'sqlite:///blog.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
